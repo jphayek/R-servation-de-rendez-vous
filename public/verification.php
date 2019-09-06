770b23a651d67ec04a7b6abb3477f2170cff7df0
jphayek <jp@MSI.localdomain>
2019-09-06 16:28:08 +0200

    some-verification.php

--- public/verification.php
+++ public/verification.php
@@ -8,8 +8,22 @@ $request_status = $_POST['request_status'];
 
 if(empty($requested_day) || empty($requested_time) || empty($requested_email) || empty($request_status))
 {
-    echo "You did not fill out the required fields.";
+    echo "You did not fill out the required fields.\n";
 }
 
+if(empty($requested_time)){
+    echo "You didn't choose a TIME! How does THE DOCTOR know ?\n";
+
+}
+
+if(empty($requested_email)){
+    echo "You must give us your E-MAIL ! How does THE DOCTOR know ?\n";
+
+}
+
+if(empty($request_status)){
+    echo "You didn't Tell us what is your problem! How does THE DOCTOR know ?\n";
+
+}
 
 ?>
