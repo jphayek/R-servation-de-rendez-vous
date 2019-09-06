6410d502076678997f7ed255df25223812d0fd03
jphayek <jp@MSI.localdomain>
2019-09-06 15:09:38 +0200

    modification-db

--- database/reservation.sql
+++ database/reservation.sql
@@ -1,9 +1,8 @@
--- phpMyAdmin SQL Dump
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 05, 2019 at 07:23 PM
+-- Generation Time: Sep 06, 2019 at 01:07 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -35,34 +34,22 @@ DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE
 IF NOT EXISTS `reservation`
 (
-  `Client1` int
-(11) NOT NULL,
-  `Client2` int
-(11) NOT NULL,
-  `Client3` int
-(11) NOT NULL,
-  `Client4` int
-(11) NOT NULL
+  `Patient1` int
+(11) NOT NULL COMMENT 'Patient1',
+  `Patient2` int
+(20) NOT NULL COMMENT 'Patient2',
+  `Patient3` int
+(11) NOT NULL COMMENT 'Patient3',
+  `Patient4` int
+(30) NOT NULL COMMENT 'Patient4',
+  UNIQUE KEY `Patient1`
+(`Patient1`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-SET SQL_MODE
-= "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT
-= 0;
-START TRANSACTION;
-SET time_zone
-= "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `doctor-jp`
